4c1c5c86b676a2a573c9de8681fe451221a39f3a
tannu945 <tanishaagrawal2015@gmail.com>
2022-11-22 17:57:46 +0530

    feat: added states and transitions in json file

--- python/action-server/state_machine.py
+++ python/action-server/state_machine.py
@@ -1,50 +1,18 @@
+#from transitions.extensions import GraphMachine
+#from functools import partial
 from transitions import Machine
-from transitions.extensions import GraphMachine
-from functools import partial
 import logging
 import asyncio
+import json
 log = logging.getLogger(__name__)
 
-class HealthyStateMachine:
+class StateMachine:
 
-    states = [{'name':'idle'},
-                    {'name':'busy'},
-                    {'name':'aborted'},
-                    {'name':'done'},
-                    {'name':'clearance_timeout'},
-                    {'name':'awaiting_clearance'}]
-    
-    transitions = [{'trigger':'start', 'source':'idle', 'dest':'busy', 'conditions':'done', 'unless':'abort'},
-                        {'trigger':'abort', 'source':'busy', 'dest':'aborted'},
-                        {'trigger':'done', 'source':'busy', 'dest':'done'},
-                        {'trigger':'awaiting_clearance', 'source':'done', 'dest':'awaiting_clearance'},
-                        {'trigger':'clearance_timeout', 'source':'awaiting_clearance', 'dest':'clearance_timeout'},
-                        {'trigger':'', 'source':'awaiting_clearance', 'dest':'idle'}]
-
-class UnhealthyStateMachine:
-
-    ustates = [{'name':'awaiting_clearance_err'},
-                    {'name':'cleared'},
-                    {'name':'broken_with_holdings'},
-                    {'name':'broken_without_holdings'},
-                    {'name':'dead'}]
-
-    utransitions = [{'trigger':'awaiting_clearance_err', 'source':'aborted', 'dest':'awaiting_clearance_err'},
-                        {'trigger':'awaiting_clearance_err', 'source':'clearance_timeout', 'dest':'awaiting_clearance_err'},
-                        {'trigger':'cleared', 'source':'awaiting_clearance_err', 'dest':'cleared'},
-                        {'trigger':'broken_without_holdings', 'source':'cleared', 'dest':'broken_without_holdings'},
-                        {'trigger':'broken_with_holdings', 'source':'awaiting_clearance_err', 'dest':'broken_with_holdings'},
-                        {'trigger':'dead', 'source':'broken_with_holdings', 'dest':'dead'},
-                        {'trigger':'dead', 'source':'broken_without_holdings', 'dest':'dead'}
-                        ]
-
-class StateMachine(HealthyStateMachine, UnhealthyStateMachine):
-    states = HealthyStateMachine.states + UnhealthyStateMachine.ustates
-    transitions = HealthyStateMachine.transitions + UnhealthyStateMachine.utransitions
-    
     def __init__(self, session):
         self.session = session
-        self.machine = Machine(model=self, states=StateMachine.states, transitions=StateMachine.transitions, initial='idle')
+        self.config = json.loads('statechart.json')
+        self.config['model'] = self
+        self.machine = Machine(**self.config)
 
     async def idle(self):
         value = ''
