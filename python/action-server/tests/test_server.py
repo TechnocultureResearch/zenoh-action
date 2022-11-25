734fc0901b087621eaeeb4772710d903fc013835
Tanisha <tanishaagrawal2015@gmail.com>
2022-11-25 18:51:54 +0530

    Update and rename test_statechart.py to test_server.py

--- python/action-server/tests/test_server.py
+++ python/action-server/tests/test_server.py
@@ -1,10 +1,11 @@
+
 from main import Session
 from state_machine import Session_state
 import zenoh
 from zenoh import QueryTarget
 import json
 
-def test_statechart():
+def get(endpoint):
     target = {
     'ALL': QueryTarget.ALL(),
     'BEST_MATCHING': QueryTarget.BEST_MATCHING(),
@@ -12,12 +13,22 @@ def test_statechart():
 
     session_obj = Session()
     session_obj.setup_action_server()
-    statechart=session_obj.session.get("Genotyper/1/DNAsensor/1/statechart", zenoh.ListCollector(), target=target)
+    statechart=session_obj.session.get("Genotyper/1/DNAsensor/1"+endpoint, zenoh.ListCollector(), target=target)
     for reply in statechart():
         try:
             value = reply.ok.payload.decode("utf-8")
         except:
             print(">> Received (ERROR: '{}')"
                 .format(reply.err.payload.decode("utf-8")))
-    
+    return value
+
+def test_statechart():
+    value = get("/statechart")
     assert json.loads(value) == json.loads(Session_state().statechart())
+
+def test_state():
+    value = get("/state")
+
+def test_trigger():
+    value = get("/trigger")
+    
