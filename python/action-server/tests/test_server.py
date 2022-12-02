16c8e5414ec404d4582fb3fa34d7bb75373f5855
tannu945 <tanishaagrawal2015@gmail.com>
2022-12-02 15:02:51 +0530

    feat: update tests

--- python/action-server/tests/test_server.py
+++ python/action-server/tests/test_server.py
@@ -5,6 +5,8 @@ from validators import ZenohConfig
 import json
 from server import Session
 from state_machine import BaseStateMachine
+from pydantic import ValidationError
+from result import Ok, Err
 
 model = Session()
 
@@ -24,21 +26,35 @@ target = {
 
 zenoh.init_logger()
 session = zenoh.open(conf)    
-
-payload = {'reponse_code':'accepted', 'message':'Trigger is accepted and triggered'}
 statemachine = BaseStateMachine()
 
 @pytest.mark.parametrize("test_input, expected", 
                             [
-                                ('/trigger?timestamp=1669897025.0759895&event=start', json.dumps(payload)),
-                                ('/trigger?timestamp=1669897025.0759895&event=abort', json.dumps(payload)),
-                                pytest.param("/statechart", json.dumps(statemachine.markup), marks=pytest.mark.xfail)
+                                ("/statechart", json.dumps(statemachine.markup, indent=3))
                             ])
-def test_server_endpoints(test_input, expected):
+def test_server_endpoint_statechart(test_input, expected):
     replies = session.get(args.base_key_expr+test_input, zenoh.ListCollector(), target=target)
     for reply in replies():
         try:
             value = reply.ok.payload.decode('utf-8')
         except:
-            print('error')
+            print(reply.err.payload.decode('utf-8'))
+    assert value == expected
+
+@pytest.mark.parametrize("test_input, expected", 
+                            [
+                                ('/trigger?timestamp=1669897025.0759895&event=start', json.dumps({'reponse_code':'accepted', 'message':'Trigger is accepted and triggered'})),
+                                ('/trigger?timestamp=1669897025.0759895&event=abort', json.dumps({'reponse_code':'accepted', 'message':'Trigger is accepted and triggered'})),
+                                ('/trigger?timestamp=1669899025.0759895&event=done', json.dumps({"reponse_code": "rejected", "message": "1 validation error for EventModel\nevent\n  Event is not valid or you are not allowed to trigger event. (type=value_error)"})),
+                                ('/trigger?timestamp=1669899025.0759895&event=clearancetimeout', json.dumps({"reponse_code": "rejected", "message": "1 validation error for EventModel\nevent\n  Event is not valid or you are not allowed to trigger event. (type=value_error)"}))
+                            ])
+def test_server_endpoints_trigger(test_input, expected):
+    replies = session.get(args.base_key_expr+test_input, zenoh.ListCollector(), target=target)
+    for reply in replies():
+        try:
+            value = reply.ok.payload.decode('utf-8')
+        except ValidationError:
+            value = reply.err.payload.decode('utf-8')
+        except ValueError:
+            value = reply.err.payload.decode('utf-8')
     assert value == expected
