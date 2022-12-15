7b0cb4663a33a10a48b654f8a60a334e45d983e0
tannu945 <tanishaagrawal2015@gmail.com>
2022-12-15 18:54:45 +0530

    reafactor: app.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Graphviz from 'graphviz-react';
 
 import action from "./data/action.json";
+import generateDotFile from "./data/generateDotFile";
 
 const endpointsList = new Set(["trigger", "statechart", "state"]);
 
@@ -24,6 +25,18 @@ function endpoint_url(endpoint = "") {
     return `${url}/${key_expression}/${endpoint}`;
 }
 
+const getStatechart = async () => {
+    try {
+        const response = await axios.get(endpoint_url("statechart"));
+        console.log(response)
+        //return response.data[0].value.message;
+
+    } catch (error) {
+        toast.error(error.message);
+        throw error;
+    }
+};
+
 const ActionComponent = (statechart, setStatechart) => {
     let [currentState, setState] = useState("Unknown");
 
@@ -41,7 +54,6 @@ const ActionComponent = (statechart, setStatechart) => {
     const postAction = async (action, handler = () => { }) => {
         try {
             const response = await axios.get(endpoint_url(action));
-            console.log(response.data[0].value)
             toast.success(`Action dispatched: ${action}\n${response.data}`);
 
             if (handler) {
@@ -53,149 +65,30 @@ const ActionComponent = (statechart, setStatechart) => {
             throw error;
         }
     };
-    const getStatechart = async (setStatechart, currentState) => {
-        try {
-            const response = await axios.get(endpoint_url("statechart"));
-            console.log(response.data[0].value);
-            setStatechart(generateDotFile(response.data[0].value, currentState));
-        } catch (error) {
-            toast.error(error.message);
-            throw error;
-        }
-    };
-
+    let currentTime = Math.floor(Date.now() / 1000);;
     return (
         <div>
             <h3>Status</h3>
             <p>{ currentState }</p>
 
             <h3>Action Buttons</h3>
-            <button onClick={ () => postAction(`trigger?timestamp=${Date.now()}&event=start`) }>Start</button>
-            <button onClick={ () => postAction(`trigger?timestamp=${Date.now()}&event=abort`) }>Stop</button>
+            <button onClick={ () => postAction(`trigger?timestamp=${currentTime}&event=start`) }>Start</button>
+            <button onClick={ () => postAction(`trigger?timestamp=${currentTime}&event=abort`) }>Stop</button>
             <button onClick={ () => postAction('state', setState) }>State</button>
-            <button onClick={ () => getStatechart(statechart, setStatechart) }>Statechart</button>
+            <button onClick={ () => setStatechart(getStatechart()) }>Statechart</button>
         </div>
     );
 };
 
 
-function states(json) {
-    let _states = {};
-
-    if (json.states === undefined) {
-        return _states;
-    }
-
-    if (json.states === {}) {
-        return _states;
-    }
-
-    json.states.forEach(state => {
-        _states[state.name] = (state.children && state.children.length !== 0) || false;
-
-        if (state.children) {
-            state.children.forEach(child => {
-                _states[child.name] = (child.children && child.children.length !== 0) || false;
-            });
-        }
-    });
-
-    return _states;
-}
-
-function transitions(json) {
-    let _transitions = [];
-
-    if (json.transitions === undefined || json.states === undefined) {
-        return _transitions;
-    }
-
-    if (json.transitions === {} || json.states === {}) {
-        return _transitions;
-    }
-
-    json.transitions.forEach(transition => {
-        _transitions.push(transition);
-    });
-
-    json.states.forEach(state => {
-        state.children && state.children.forEach(substate => {
-            substate.transitions && substate.transitions.forEach(transition => {
-                _transitions.push(transition);
-            });
-        });
-    });
-
-    // console.log(_transitions);
-    return _transitions;
-}
-
-function childtransitionToStr(json, cluster) {
-    let child_transition = "";
-    json.states.forEach(state => {
-        if (cluster === state.name) {
-            state.transitions.forEach(transition => {
-                child_transition += transitionToStr(transition, cluster);
-            });
-        }
-    });
-    return child_transition;
-}
-
-function transitionToStr(transition, state_list) {
-    let cluster_label = "";
-    if (state_list[transition.dest]) {
-        cluster_label += `, ltail=${transition.dest}`;
-    }
-    if (state_list[transition.source]) {
-        cluster_label += `, lhead=${transition.source}`;
-    }
-    return `"${transition.source}" -> "${transition.dest}" [label="${transition.trigger} ${cluster_label}"]\n`;
-}
-
-function generateDotFile(json, current_state) {
-    let dot = "";
-
-    if (json === undefined) {
-        return dot;
-    }
-
-    dot += "digraph {\n";
-    dot += `rankdir=LR;\n`;
-    dot += `Entry [shape="point" label=""]`;
-    dot += `Entry -> ${json.initial}\n`;
-    dot += `${json.initial} [shape=ellipse, color=red, fillcolor= orangered3, fontcolor=black, style=filled]; \n`;
-
-    // Approach 1: Build some data structures, then draw
-    let state_dict = states(json);
-    let transition_list = transitions(json);
-
-    const clusters = [];
-    Object.entries(state_dict)
-        .filter(([k, v]) => v === true)
-        .forEach(arr => clusters.push(arr[0]));
-
-
-    clusters.forEach(cluster => {
-        dot += `subgraph cluster_${cluster} {\n`;
-        dot += `${current_state.split("_").pop()} [shape=ellipse, color=lightsalmon, fillcolor=lightsalmon, fontcolor=black, style=filled]; \n`;
-        dot += childtransitionToStr(json, cluster);
-        dot += ` label=${cluster}\n`;
-
-        dot += `}\n`;
-    });
-
-    transition_list.forEach(transition => {
-        dot += transitionToStr(transition, state_dict);
-    });
-
-    dot += "}\n";
-    return dot;
-}
-
-function App() {
-    const [statechart, setStatechart] = useState({});
-    const dot = generateDotFile(statechart, "idle");
+function App(){
+    let dot = generateDotFile({});
+    const [statechart, setStatechart] = useState("Unknown");
+    console.log(setStatechart);
+    useEffect(()=>{
+        dot = generateDotFile(statechart);
+        return () => dot;
+    }, [statechart]);
     return (
         <div className="App">
             <header className="App-header">
@@ -213,7 +106,7 @@ function App() {
                 pauseOnHover={ false }
             />
             <ActionComponent statechart={ statechart } setStatechart={ setStatechart } />
-            <Graphviz dot = {dot} options={ { width: 1000, height: 1000 } } />
+           
         </div>
     );
 }
