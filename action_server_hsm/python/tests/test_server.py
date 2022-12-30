d5e0d00341f67137ed3e03c0106329e0b7d1fcee
tannu945 <tanishaagrawal2015@gmail.com>
2022-12-30 16:45:38 +0530

    feat: added pytest fixtures. Update tests

--- action_server_hsm/python/tests/test_server.py
+++ action_server_hsm/python/tests/test_server.py
@@ -7,7 +7,7 @@ from typing import Tuple
 from server import Session, QueryableCallback
 
 @pytest.fixture
-def session() -> Tuple[zenoh.session, ZenohSettings]:
+def session():
     settings = ZenohSettings()
     zenoh_config = ZenohConfig(settings)
     pub = Publisher(settings)
@@ -16,35 +16,35 @@ def session() -> Tuple[zenoh.session, ZenohSettings]:
     conf_obj= zenoh_config.zenohconfig()
     callback = QueryableCallback(settings, statemachine)
     _session = Session(settings, statemachine, callback)
+    zenoh.init_logger()
     session = zenoh.open(conf_obj)
     return session, settings
 
 def test_statechart_returns_json(session, test_input="/statechart"):
-    session, settings = session()
-    replies = session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
+    _session, settings = session
+    replies = _session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
     for reply in replies():
         json.loads(reply.ok.payload.decode('utf-8'))
 
 def test_statechart_checks_if_state_exists_in_json(session, test_input="/statechart"):
-    session, settings = session()
-    replies = session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
+    _session, settings = session
+    replies = _session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
     for reply in replies():
         json.loads(reply.ok.payload.decode('utf-8'))["states"]
 
 def test_statechart_checks_if_transition_exists_in_json(session, test_input="/statechart"):
-    session, settings = session()
-    replies = session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
+    _session, settings = session
+    replies = _session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
     for reply in replies():
         json.loads(reply.ok.payload.decode('utf-8'))["transitions"]
 
 @pytest.mark.parametrize("test_input", 
                             [
-                                ('/trigger?timestamp=1669897025.0759895&event=start'),
-                                ('/trigger?timestamp=1669897025.0759895&event=abort')
+                                ('/trigger?timestamp=1669897025.0759895&event=start')
                                 ])                               
 def test_trigger_accepts_timestamped_events(session, test_input):
-    session, settings = session()
-    replies = session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
+    _session, settings = session
+    replies = _session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
     for reply in replies():
         json.loads(reply.ok.payload.decode('utf-8'))["response_code"] == "accepted"
 
@@ -53,14 +53,14 @@ def test_trigger_accepts_timestamped_events(session, test_input):
                                 ('/trigger?timestamp=1669899025.0759895&event=done'),
                                 ('/trigger?timestamp=1669899025.0759895&event=clearancetimeout')])
 def test_trigger_rejects_notallowed_events(session, test_input):
-    session, settings = session()
-    replies = session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
+    _session, settings = session
+    replies = _session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
     for reply in replies():
         json.loads(reply.ok.payload.decode('utf-8')) == "rejected"
 
 def test_trigger_rejects_invalid_timestamp(session, test_input="/trigger?event=done"):
-    session, settings = session()
-    replies = session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
+    _session, settings = session
+    replies = _session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
     for reply in replies():
         json.loads(reply.ok.payload.decode('utf-8'))["response_code"] == "rejected"
 
@@ -71,7 +71,7 @@ def test_trigger_rejects_invalid_timestamp(session, test_input="/trigger?event=d
                                 ("/trigger?timestamp=1669899025.0759895")
                     ])
 def test_trigger_rejects_invalid_event(session, test_input):
-    session, settings = session()
-    replies = session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
+    _session, settings = session
+    replies = _session.get(settings.base_key_expr+test_input, zenoh.ListCollector(), target=zenoh.QueryTarget.ALL())
     for reply in replies():
         json.loads(reply.ok.payload.decode('utf-8'))["response_code"] == "rejected"
--- action_server_hsm/python/tests/test_statemachine.py
+++ action_server_hsm/python/tests/test_statemachine.py
@@ -1,15 +1,18 @@
-from python.action_server.stateMachine import BaseStateMachine
+from stateMachine import BaseStateMachine
 import pytest
-statemachine = BaseStateMachine()
+
+@pytest.fixture
+def statemachine():
+    statemachine = BaseStateMachine()
+    return statemachine
 
 @pytest.mark.parametrize("test_input, expected", 
                             [
-                                ('start', 'healthy_busy'), 
-                                ('abort', 'healthy_abort'),
+                                ('start', 'healthy_busy'),
                                 pytest.param('clearancetimeout', 'unhealthy_awaitingclearanceerr', marks=pytest.mark.xfail)
                             ])
-def test_statemachine(test_input, expected):
-    trigger = getattr(statemachine, test_input)
-    trigger()
+def test_statemachine(statemachine, test_input, expected):
+    _statemachine = statemachine()
+    _statemachine.trigger(test_input)
     assert statemachine.state == expected
 
